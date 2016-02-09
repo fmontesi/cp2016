dd8767a53ebf364ac61ac85c3bec37a370e0019b
fmontesi <famontesi@gmail.com>
2016-02-09 12:47:43 +0100

    Import exercises 1-6

---
+++ exercises/src/cp/week6/Exercise1.java
@@ -0,0 +1,14 @@
+package cp.week6;
+
+/**
+ *
+ * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
+ */
+public class Exercise1
+{
+	/*
+	- Create a Counter class storing an integer (a field called i), with an increment and decrement method.
+	- Make Counter Thread-safe.
+	- Does it make a different to declare i private or public?
+	*/
+}
