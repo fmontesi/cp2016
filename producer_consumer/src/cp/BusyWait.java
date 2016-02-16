0b7e6cad3d5c2fde556db9a58d69ddc9528c1d7f
Fabrizio Montesi <famontesi@gmail.com>
2016-02-16 08:21:06 +0100

    Add sequential and busywait to producer_consumer.

--- producer_consumer/src/cp/BusyWait.java
+++ producer_consumer/src/cp/BusyWait.java
@@ -1,14 +1,88 @@
 package cp;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.IntStream;
+
 /**
  *
  * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
  */
 public class BusyWait
 {
+	private static class Product {
+		private final String name;
+		private final String attributes;
+		public Product( String name, String attributes )
+		{
+			this.name = name;
+			this.attributes = attributes;
+		}
+		
+		public String toString()
+		{
+			return name + ". " + attributes;
+		}
+	}
+	private static final Deque< Product > THE_LIST = new LinkedList<>();
+	
+	private static void produce( Deque< Product > list, String threadName )
+	{
+		IntStream.range( 1, 200 ).forEach( i -> {
+			synchronized( list ) {
+				Product prod = new Product( "Water Bottle", "Liters: " + i + ". By thread: " + threadName );
+				list.add( prod );
+				System.out.println( threadName + " producing " + prod );
+			}
+			synchronized( list ) {
+				Product prod = new Product( "Flower Bouquet", "Amount: " + i + ". By thread: " + threadName );
+				list.add( prod );
+				System.out.println( threadName + " producing " + prod );
+				
+			}
+		} );
+	}
+	
+	private static void consume( Deque< Product > list, String threadName, CountDownLatch latch )
+	{
+		boolean keepRun = true;
+		while( keepRun ) {
+			synchronized( list ) {
+				if ( !list.isEmpty() ) {
+					Product prod = list.removeFirst();
+					System.out.println( threadName + " consuming " + prod.toString() );
+				} else if ( latch.getCount() == 0 ) {
+					keepRun = false;
+				}
+			}
+		}
+	}
+	
+	private static final int NUM_PRODUCERS = 3;
+	
 	public static void run()
 	{
-		// A factory produces some products to be packaged
-		// Producer-Consumer
+		CountDownLatch latch = new CountDownLatch( NUM_PRODUCERS );
+		IntStream.range( 0, NUM_PRODUCERS ).forEach(
+		i -> {
+			new Thread( () -> {
+				produce( THE_LIST, "Producer" + i );
+				latch.countDown();
+			} ).start();
+			new Thread( () -> {
+				consume( THE_LIST, "Consumer" + i, latch );
+			} ).start();
+		} );
+		
+		/*
+		IntStream.range( 0, NUM_PRODUCERS ).forEach( i -> { ... } );
+		
+		does the same thing as
+		
+		for( int i = 0; i < NUM_PRODUCERS; i++ ) {
+			...
+		}
+		*/
 	}
 }
--- producer_consumer/src/cp/Sequential.java
+++ producer_consumer/src/cp/Sequential.java
@@ -1,14 +1,58 @@
 package cp;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.stream.IntStream;
+
 /**
  *
  * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
  */
 public class Sequential
 {
+	private static class Product {
+		private final String name;
+		private final String attributes;
+		public Product( String name, String attributes )
+		{
+			this.name = name;
+			this.attributes = attributes;
+		}
+		
+		public String toString()
+		{
+			return name + ". " + attributes;
+		}
+	}
+	private static final Deque< Product > THE_LIST = new LinkedList<>();
+	
+	private static void produce( Deque< Product > list )
+	{
+		IntStream.range( 1, 1000 ).forEach( i -> {
+			list.add( new Product( "Water Bottle", "Liters: " + i ) );
+			list.add( new Product( "Flower Bouquet", "Amount: " + i ) );
+		} );
+	}
+	
+	private static void consume( Deque< Product > list )
+	{
+		while( !list.isEmpty() ) {
+			Product prod = list.removeFirst();
+			System.out.println( prod ); // Equivalent to System.out.println( prod.toString() );
+		}
+	}
+	
 	public static void run()
 	{
-		// A factory produces some products to be packaged
-		// Producer-Consumer
+		produce( THE_LIST );
+		consume( THE_LIST );
+		
+		/*
+		The above does the same thing as:
+		
+		for( int i = 0; i < 1000; i++ ) {
+			THE_LIST.add( "Product number " + i );
+		}
+		*/
 	}
 }
