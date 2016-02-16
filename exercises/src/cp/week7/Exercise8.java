a07dcc837f691e62ac0f054012e277e506b2673b
fmontesi <famontesi@gmail.com>
2016-02-16 08:40:36 +0100

    Exercises 7-9.

---
+++ exercises/src/cp/week7/Exercise8.java
@@ -0,0 +1,12 @@
+package cp.week7;
+
+/**
+ *
+ * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
+ */
+public class Exercise8
+{
+	/*
+	- Modify producer_consumer/BusyWait to start a number of consumers that is half the number of producers.
+	*/
+}
---
+++ exercises/src/cp/week7/Exercise9.java
@@ -0,0 +1,15 @@
+package cp.week7;
+
+/**
+ *
+ * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
+ */
+public class Exercise9
+{
+	/*
+	- Modify producer_consumer/BusyWait such that each producer randomly decides
+	  the number of elements that it will puts in the list.
+	- Hint: use java.util.Random. Documentation: https://docs.oracle.com/javase/7/docs/api/java/util/Random.html
+	- Make it so the number of elements produced by each producer cannot exceed 10000.
+	*/
+}
