23cbf09021b9cc6207aa97dcc4adeb5bbba05b64
fmontesi <famontesi@gmail.com>
2016-03-09 08:19:57 +0100

    Update.

--- exercises/src/cp/week10/Exercise21.java
+++ exercises/src/cp/week10/Exercise21.java
@@ -10,9 +10,9 @@ public class Exercise21
 	! (Exercises marked with ! are harder than usual)
 	
 	- Make it so the user simply needs to invoke a static method, e.g., "count" to start the word counting of an array of strings.
-	- count should check if the array is bigger than 10; if so, it should split the array in two and invoke count on these
-	two slices in parallel (using an executor).
-	- when the two sub-computations are done, collect the results.
+	- Method count should check if the array is bigger than 10; if so, it should split the array in two and invoke count on these
+	  two slices in parallel (using an executor).
+	- When the two sub-computations are done, collect the results.
 	- At the end, output the total count of words.
 	*/
 }
