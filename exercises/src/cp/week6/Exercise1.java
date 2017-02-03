b200f15256f179e606e090888b826a486cf35130
Fabrizio Montesi <famontesi@gmail.com>
2017-02-03 14:42:38 +0100

    Update Exercise1.java

--- exercises/src/cp/week6/Exercise1.java
+++ exercises/src/cp/week6/Exercise1.java
@@ -9,6 +9,6 @@ public class Exercise1
 	/*
 	- Create a Counter class storing an integer (a field called i), with an increment and decrement method.
 	- Make Counter Thread-safe.
-	- Does it make a different to declare i private or public?
+	- Does it make a difference to declare i private or public?
 	*/
 }
